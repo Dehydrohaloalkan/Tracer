e3ef24eea72fd3a6e5b8d43c40757378483bcf87
alexb <alexbondar03@gmail.com>
2022-09-18 18:15:24 +0300

    Add Example and Tests projects

---
+++ Tracer/Tracer.Core/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Tracer.Core
+{
+    public class Tracer : ITracer
+    {
+        public void StartTrace()
+        { 
+            
+        }
+
+        public void StopTrace()
+        {
+            
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tracer/Tracer.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tracer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
