8f31e39f221ab6ca5aafd55af46143c429357198
alexb <alexbondar03@gmail.com>
2022-09-25 21:09:05 +0300

    Add tracerInfo

---
+++ Tracer/Tracer.Core/TracerInfo/MethodInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Tracer.Core.TracerInfo
+{
+    internal class MethodInfo
+    {
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+        public Stopwatch Stopwatch { get; set; }
+        public List<MethodInfo> Methods { get; set; }
+
+        public MethodInfo()
+        {
+            Name = String.Empty;
+            ClassName = String.Empty;
+            Stopwatch = new Stopwatch();
+            Methods = new List<MethodInfo>();
+        }
+    }
+}
---
+++ Tracer/Tracer.Core/TracerInfo/ThreadInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace Tracer.Core.TracerInfo
+{
+    internal class ThreadInfo
+    {
+        public ConcurrentStack<MethodInfo> RunningMethods { get; set; }
+        public List<MethodInfo> TopMethods { get; set; }
+
+        public ThreadInfo()
+        {
+            RunningMethods = new ConcurrentStack<MethodInfo>();
+            TopMethods = new List<MethodInfo>();
+        }
+    }
+}
