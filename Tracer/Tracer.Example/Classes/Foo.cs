f9f131fec9c235564d069c67744b0061b68e8544
alexb <alexbondar03@gmail.com>
2022-09-25 22:25:03 +0300

    Add example

---
+++ Tracer/Tracer.Example/Classes/Foo.cs
@@ -0,0 +1,26 @@
+﻿using Tracer.Core;
+
+namespace Tracer.Example.Classes
+{
+    public class Foo
+    {
+        private readonly Bar _bar;
+        private readonly ITracer _tracer;
+
+        public Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(200);
+            _bar.InnerMethod();
+            Thread.Sleep(200);
+            _tracer.StopTrace();
+        }
+
+    }
+}
