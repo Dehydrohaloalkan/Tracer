50a3e08295f896d17e9d5852ff12ea1f39e8dd2a
alexb <alexbondar03@gmail.com>
2022-09-25 22:09:41 +0300

    Add logic to Tracer

--- Tracer/Tracer.Core/Tracer.cs
+++ Tracer/Tracer.Core/Tracer.cs
@@ -1,22 +1,54 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+using Tracer.Core.TracerInfo;
+using Tracer.Core.TracerResult;
 
 namespace Tracer.Core
 {
     public class Tracer : ITracer
     {
+        private readonly ConcurrentDictionary<int, ThreadInfo> _threads = new();
+
         public void StartTrace()
-        { 
-            
+        {
+            var stackMethod = new StackTrace().GetFrame(1)!.GetMethod()!;
+
+            MethodInfo methodInfo = new MethodInfo()
+            {
+                Name = stackMethod.Name,
+                ClassName = stackMethod.DeclaringType != null ? stackMethod.DeclaringType.Name : string.Empty
+            };
+
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            var threadInfo = _threads.GetOrAdd(threadId, _ => new ThreadInfo());
+
+            if (threadInfo.RunningMethods.Count > 0)
+            {
+                var parent = threadInfo.RunningMethods.First();
+                parent.Methods.Add(methodInfo);
+            }
+            else
+            {
+                threadInfo.TopMethods.Add(methodInfo);
+            }
+
+            threadInfo.RunningMethods.Push(methodInfo);
+            methodInfo.Stopwatch.Start();
+
         }
 
         public void StopTrace()
         {
-            
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            if (!_threads[threadId].RunningMethods.TryPop(out var methodInfo))
+                return;
+            methodInfo.Stopwatch.Stop();
         }
 
         public TraceResult GetTraceResult()
         {
-            throw new NotImplementedException();
+            return new TraceResult(_threads.Values.Select((t, index) => new TraceThread(index, t)).ToList());
         }
     }
 }
