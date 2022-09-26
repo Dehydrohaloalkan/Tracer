308b86e2cc9ca6ec3a2ddca2c357533400eed70f
alexb <alexbondar03@gmail.com>
2022-09-26 12:23:10 +0300

    Add Serializers Manager

---
+++ Tracer/Tracer.Serialization/SerializersManager.cs
@@ -0,0 +1,18 @@
+﻿using Tracer.Core.TracerResult;
+using Tracer.Serialization.Abstraction;
+
+namespace Tracer.Serialization
+{
+    public static class SerializersManager
+    {
+        public static void SerializeToFiles(List<ITraceResultSerializer> serializers, TraceResult traceResult,
+            string fileName)
+        {
+            foreach (var serializer in serializers)
+            {
+                using var fileStream = new FileStream( $"{fileName}.{serializer.Format}", FileMode.Create);
+                serializer.Serialize(traceResult, fileStream);
+            }
+        }
+    }
+}
